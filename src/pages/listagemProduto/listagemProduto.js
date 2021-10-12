96a126ee521e78badd0aa01f3472cc96018907c6
Bruna de Assuncao <bruna.assuncao@olxbr.com>
2021-10-11 21:40:04 -0300

    criacao pag listagem prod react

---
+++ src/pages/listagemProduto/listagemProduto.js
@@ -0,0 +1,42 @@
+import "./listagemProduto.css"
+
+import HeaderSecundario from "../../components/header/HeaderSecundario"
+import Footer from "../../components/footer/Footer"
+
+function PaginaListagemProduto(){
+    return(
+        <div>
+            <HeaderSecundario/>
+            <Footer/>
+        </div>
+    )
+}
+
+function Primeira(){
+    return(
+        <p>codigo html aqui</p>
+    )
+}
+
+function Segunda(){
+    return(
+        
+        <p>codigo html aqui</p>
+    )
+}
+
+function Terceira(){
+    return(
+
+        <p>codigo html aqui</p>
+    )
+}
+
+function Quarta(){
+    return(
+
+        <p>codigo html aqui</p>
+    )
+}
+
+export default PaginaListagemProduto
