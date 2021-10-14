8a9d150c4082e0d96e1abb42140c3fdd869e5139
JuliaMendes <juliadepaivams@gmail.com>
2021-10-14 14:38:16 -0300

    alterando para css module

--- src/pages/cadastroUsuario/CadastroUsuario.js
+++ src/pages/cadastroUsuario/CadastroUsuario.js
@@ -1,4 +1,4 @@
-import './cadastroUsuario.css';
+import css from './cadastroUsuario.module.css';
 
 import HeaderSimples from '../../components/headerSimples/HeaderSimples';
 import backgroundGreen from '../../img/imagens/bg.jpeg'
@@ -17,9 +17,9 @@ function PaginaCadastroUsuario() {
 
 function CardCadastrese() {
     return (
-        <main className="container">
+        <main className= {css.container}>
 
-            <section className="card-cadastre-se">
+            <section className={css["card-cadastre-se"]}>
                 <h1>Crie sua conta na&nbsp;<span>aluga</span><span id="ki">Ki</span></h1>
                 <form action="">
                     <label htmlFor="fullname">Nome completo</label><br />
@@ -30,45 +30,45 @@ function CardCadastrese() {
                     <input type="email" id="email" name="email" placeholder="meuemail@email.com" required onKeyUp={() => {}} /><br />
 
                     <label htmlFor="pass">Senha</label><br />
-                    <div className="input-icone">
+                    <div className={css["input-icone"]}>
                         <input type="password" id="pass" name="password" placeholder="Use no mínimo 8 caracteres"
                             minLength="8" maxLength="64" required onKeyUp={() => {}}/>
-                        <img className="olho" src={iconeOlho} alt="Ícone olho" />
+                        <img className={css["olho"]} src={iconeOlho} alt="Ícone olho" />
                     </div><br />
 
-                    <ul className="erros"></ul>
+                    <ul className={css["erros"]}></ul>
 
                     <hr />
 
-                    <div className="checkboxes">
+                    <div className={css["checkboxes"]}>
                         <input type="checkbox" id="novidades" name="novidades" value="novidades" defaultChecked />
                         <label htmlFor="novidades"> aceito receber novidades da alugaKi</label>
                     </div>
 
-                    <div className="checkboxes">
+                    <div className={css["checkboxes"]}>
                         <input type="checkbox" id="termos" name="termos" value="termo" defaultChecked required />
                         <label htmlFor="termos"> estou de acordo com os <span>termos de serviços</span> e as <span>politicas de
                             privacidade</span> da
                             alugaKi</label>
                     </div>
 
-                    <div className="btn-criar-conta">
+                    <div className={css["btn-criar-conta"]}>
                         <button type="submit">Criar conta</button>
                     </div>
 
                 </form>
 
-                <div className="line">
+                <div className={css["line"]}>
                     <hr />
-                    <span>OU</span>
+                    <span>ou</span>
                     <hr />
                 </div>
 
-                <div className="btn-facebook">
+                <div className={css["btn-facebook"]}>
                     <a href="https://www.facebook.com/"><button>Cadastre-se com o facebook</button></a>
                 </div>
 
-                <div className="tem-cadastro">
+                <div className={css["tem-cadastro"]}>
                     <h2>Já tem cadastro? <a href="login.html">Entrar</a></h2>
                 </div>
             </section>
