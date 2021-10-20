92759df68cd41afecec173824871d16db3971daf
JuliaMendes <juliadepaivams@gmail.com>
2021-10-20 11:44:42 -0300

    adicionando api localização

--- src/pages/paginaInicial/PaginaInicial.js
+++ src/pages/paginaInicial/PaginaInicial.js
@@ -8,9 +8,8 @@ import iconeLupa from "../../img/icones/search.png";
 import iconeSetaDireita from "../../img/icones/seta-direita.png";
 import backgroundGreen from "../../img/imagens/bg.jpeg";
 import backgroundBannerVioloes from "../../img/imagens/banner-violao.jpeg";
-import estrela from  "../../img/icones/star1.png";
-import localizacao from  "../../img/icones/location.png";
-
+import estrela from "../../img/icones/star1.png";
+import localizacao from "../../img/icones/location.png";
 
 function PaginaInicial() {
   return (
@@ -29,6 +28,40 @@ function PaginaInicial() {
 }
 
 function PartePrincipal() {
+  const [categoriaAtual, setCategoriaAtual] = useState("");
+  const [localizacaoAtual, setLocalizacaoAtual] = useState("");
+  const [buscaAtual, setBuscaAtual] = useState("");
+
+
+  useEffect(() => {
+    if (window.screen.width < 768) {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          function (position) {
+            console.log(position);
+            let latitude = position.coords.latitude;
+            console.log(latitude);
+            let longitude = position.coords.longitude;
+
+            fetch(
+              `https://geolocation.contrateumdev.com.br/api/geocode?lat=${latitude}7&lon=${longitude}`
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                let estado = data.address.state;
+                console.log(estado);
+              });
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
+      } else {
+        alert("ops, não foi possível pegar a sua localização");
+      }
+    }
+  }, []);
+
   return (
     <body className="paginaInicial">
       <section
@@ -43,7 +76,9 @@ function PartePrincipal() {
           <div className="barra-de-pesquisa">
             <div className="barra">
               <div className="opt-localizacao">
-                <select name="localizacao" id="localizacao">
+                <select onChange={(e) =>
+                    setLocalizacaoAtual(e.target.selectedOptions[0].value)
+                  } name="localizacao" id="localizacao">
                   <option selected disabled value="">
                     Localização
                   </option>
@@ -53,7 +88,13 @@ function PartePrincipal() {
               </div>
 
               <div className="opt-categoria">
-                <select name="categoria" id="categoria">
+                <select
+                  onChange={(e) =>
+                    setCategoriaAtual(e.target.selectedOptions[0].value)
+                  }
+                  name="categoria"
+                  id="categoria"
+                >
                   <option selected disabled value="">
                     Categoria
                   </option>
@@ -67,25 +108,35 @@ function PartePrincipal() {
               </div>
 
               <div className="pesquisar-produto">
-                <input
+                <input onChange={(e) =>
+                    setBuscaAtual(e.target.value)
+                  }
                   type="text"
                   name="produto"
                   id="produto"
                   placeholder="O que você está procurando?"
+                  value = {buscaAtual}
                 />
                 <div className="lupa">
-                  <button onClick="busca()">
+                  <button>
                     <img src={iconeLupa} alt="Botao de lupa" />
                   </button>
                 </div>
               </div>
             </div>
-            {/* <div className="pesquisar-produto-mobile">
-                            <input type="text" name="produto" id="produto-mobile" placeholder="O que você está procurando?" />
-                            <div className="lupa">
-                                <button onclick="buscaMobile()" ><img src={"img/search.png"} alt="Botao de lupa" /></button>
-                            </div>
-                        </div> */}
+            <div className="pesquisar-produto-mobile">
+              <input
+                type="text"
+                name="produto"
+                id="produto-mobile"
+                placeholder="O que você está procurando?"
+              />
+              <div className="lupa">
+                <button onclick="buscaMobile()">
+                  <img src={"img/search.png"} alt="Botao de lupa" />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -124,7 +175,12 @@ function ProdutosPorCategoria() {
       <section className="produtos-por-categoria">
         <div className="container">
           <div className="botoes-categoria">
-            <button className={`${categoriaAtual === 'Esporte e Lazer' ? "botoes-categoria-selecionado" : "" }`}
+            <button
+              className={`${
+                categoriaAtual === "Esporte e Lazer"
+                  ? "botoes-categoria-selecionado"
+                  : ""
+              }`}
               onClick={(e) => {
                 setcategoriaAtual(e.target.innerText);
               }}
@@ -133,7 +189,9 @@ function ProdutosPorCategoria() {
             </button>
             <span>|</span>
             <button
-                className={`${categoriaAtual === 'Moda' ? "botoes-categoria-selecionado" : "" }`}
+              className={`${
+                categoriaAtual === "Moda" ? "botoes-categoria-selecionado" : ""
+              }`}
               onClick={(e) => {
                 setcategoriaAtual(e.target.innerText);
               }}
@@ -142,7 +200,11 @@ function ProdutosPorCategoria() {
             </button>
             <span>|</span>
             <button
-            className={`${categoriaAtual === 'Eletrônicos' ? "botoes-categoria-selecionado" : "" }`}
+              className={`${
+                categoriaAtual === "Eletrônicos"
+                  ? "botoes-categoria-selecionado"
+                  : ""
+              }`}
               onClick={(e) => {
                 setcategoriaAtual(e.target.innerText);
               }}
@@ -151,7 +213,11 @@ function ProdutosPorCategoria() {
             </button>
             <span>|</span>
             <button
-            className={`${categoriaAtual === 'Ferramentas e Utilitários' ? "botoes-categoria-selecionado" : "" }`}
+              className={`${
+                categoriaAtual === "Ferramentas e Utilitários"
+                  ? "botoes-categoria-selecionado"
+                  : ""
+              }`}
               onClick={(e) => {
                 setcategoriaAtual(e.target.innerText);
               }}
